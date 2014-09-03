bfbd01ea0b07d4d7eca68aced1f6e2bbb7201909
Shahjada Md. Tanvir Ahmed <tanvir@playdom.com.bd>
2014-09-03 21:22:18 +0600

    hello

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+
 print ("HelloWorldHI")
 print ("Jmeter studying!!!")
 print ("sakib!")
@@ -5,4 +6,5 @@ print ("Tanvir")
 print ("changed by Sakib1")
 print ("Updated from my Home--This is Sakib")
 print ("world.")
-print ("Finally with some trouble...in git config from terminal managed to crack it! ")
+print ("Finally with some trouble...in git config from terminal managed to crack it! "
+print()
---
+++ pygame_basics.py
@@ -0,0 +1,8 @@
+__author__ = 'tanvirshahjada'
+
+import pygame
+import sys
+
+pygame.init()
+
+screen = pygame.display.set_mode((640,480))
